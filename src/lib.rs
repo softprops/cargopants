04f97786cfc731837ea7c5068f34064c1cd1585c
softprops <d.tangren@gmail.com>
2015-02-23 08:37:10 -0500

    remove unneeded abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ extern crate mime;
 extern crate url;
 
 use hyper::{ Client, Url };
-use hyper::header::{ Accept, ContentType, UserAgent, qitem };
+use hyper::header::{ Accept, Authorization, ContentType, UserAgent, qitem };
 use hyper::method::Method;
 use mime::{ Attr, Mime, Value };
 use mime::TopLevel::Application;
@@ -20,12 +20,6 @@ pub type Result<T> = result::Result<T, IoError>;
 pub struct Crates {
   host: String,
   token: Option<String>
-}
-
-#[derive(PartialEq, Copy)]
-pub enum Auth {
-  Authorized,
-  Unauthorized
 }
 
 #[derive(RustcDecodable)]
@@ -47,25 +41,26 @@ impl Crates {
   }
  
   pub fn search(&mut self, query: &str) -> Result<Vec<Crate>> {
-    let body = try!(self.get(format!("/crates?q={}", query), Auth::Unauthorized));
+    let body = try!(self.get(format!("/crates?q={}", query)));
     Ok(json::decode::<CrateList>(&body).unwrap().crates)
   }
 
   pub fn named(&mut self, name: &str) -> Result<String> { // can't decode this automagically because it contains the key "crate"!
-    self.get(format!("/crates/{}", name), Auth::Unauthorized)
+    self.get(format!("/crates/{}", name))
   }
 
-  fn get(&mut self, path: String, auth: Auth) -> Result<String> {
-    self.req(path, None, Method::Get, auth)
+  fn get(&mut self, path: String) -> Result<String> {
+    self.req(path, None, Method::Get)
   }
 
-  fn req(&mut self, path: String, body: Option<&[u8]>, method: Method, auth: Auth) -> Result<String> {
+  fn req(&mut self, path: String, body: Option<&[u8]>, method: Method) -> Result<String> {
      let uri = Url::parse(&format!("{}/api/v1{}", self.host, path)).ok().expect("invalid url");
+     // todo: send Authorization header when token is defined
      let mut res = match Client::new()
         .request(method, uri)
         .header(UserAgent("ro/0.1.0".to_string()))
         .header(Accept(vec![qitem(Mime(Application, Json, vec![(Attr::Charset, Value::Utf8)]))]))
-        .header(ContentType(Mime(Application, Json, vec![])))
+        .header(ContentType(Mime(Application, Json, vec![(Attr::Charset, Value::Utf8)])))
         .send() {
             Ok(r) => r,
             Err(err) => panic!("failed request: {:?}", err)
