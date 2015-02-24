f2e33ff7e0ab11d580667426a7c67015082ec29a
softprops <d.tangren@gmail.com>
2015-02-24 01:18:09 -0500

    default constructor

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate cargopants;
 use cargopants::Client;
 
 fn main() {
-   let mut crates = Client::new(
-     "https://crates.io".to_string(), None);
+   let mut crates = Client::new();
    let get = crates.named("url");
    println!("result {:?}", get);
    let query = crates.find(
